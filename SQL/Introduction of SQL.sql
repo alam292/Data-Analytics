f873c3ba8eb3cff405f9d329354649ec9a76f402
Md MatlooB Alam <123268730+alam292@users.noreply.github.com>
2026-04-15 22:50:26 +0530

    Add files via upload

---
+++ SQL/Introduction of SQL.sql
@@ -0,0 +1,20 @@
+------- What is Data ?
+Data is raw information that can be processed to get meaningful insights.
+
+Example:-
+Number ---> 10,289
+Texts ---> 'Rahul'
+Date ----> '02-11-2023'
+Yes/No ----> True/False
+
+
+------ What is Database?
+A database is an organized collection of data that can be easily accessed, managed and update.
+
+It Stores data in structured formate.
+
+Example:-
+
+
+
+
